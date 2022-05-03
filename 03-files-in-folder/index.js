6daf6befaf4b55298b204cfdd5d7e869056c3125
Oleg Ganin <olejaaaa@gmail.com>
2022-05-03 15:46:39 +0300

    fix: 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,15 +4,15 @@ const path = require('path');
 const secretFolder = path.join(__dirname, 'secret-folder');
 
 async function readDir(dir){
-  const files = await (await fs.promises.readdir(dir, {withFileTypes: true}))
+  await (await fs.promises.readdir(dir, {withFileTypes: true}))
     .filter(el => el.isFile())
-    .map(el => el.name);
-  files.forEach(el => {
-    fs.stat(path.join(dir, el), (err, stats) => {
-      if (err) throw err;
-      console.log(`<${path.parse(el).name}>-<${path.extname(el).slice(1)}>-<${stats.size}b>`);}
-    );
-  });
+    .map(el => el.name)
+    .forEach(el => {
+      fs.stat(path.join(dir, el), (err, stats) => {
+        if (err) throw err;
+        console.log(`<${path.parse(el).name}>-<${path.extname(el).slice(1)}>-<${stats.size}b>`);}
+      );
+    });
 }
 
 readDir(secretFolder);
